89f5fdeb35ce8ea6db4b52d5c0716524acfc85e0
Emiree69 <122453837+Emiree69@users.noreply.github.com>
2023-09-08 16:10:19 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 // Define an array of page URLs
-const pageURLs = {
+let pageURLs = {
     page1: "pages/aboutpage.html",
-    page3: "pages/contact.html"
+    page2: "pages/contact.html",
+    page3: "bonus/bonus.php"
 };
 
-// Get all elements with the class "navigateButton" and convert them to an array
-const buttons = Array.from(document.querySelectorAll(".navigateButton"));
+let buttons = Array.from(document.querySelectorAll(".navigateButton"));
 
-// Add a click event listener to each button in the array
 buttons.forEach(button => {
     button.addEventListener("click", function() {
-        // Get the data-page attribute value to determine which page to navigate to
-        const targetPage = this.getAttribute("data-page");
+        let targetPage = this.getAttribute("data-page");
 
-        // Use the targetPage to get the corresponding URL from the pageURLs object
-        const newPageURL = pageURLs[targetPage];
+        let newPageURL = pageURLs[targetPage];
 
-        // Use window.location to navigate to the new page
         window.location.href = newPageURL;
     });
 });
 
+//Using a for loop
+for (let i = 0; i < 10; i++) {
+    console.log("Hello, welcome to my site");
+  }
